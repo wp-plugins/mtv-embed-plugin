800896858a4fcb040e5507a7ca692ae3cf886961
BaconCZE <BaconCZE@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-15 12:52:30 +0000

    Created branch 1.1.
    git-svn-id: https://plugins.svn.wordpress.org/mtv-embed-plugin/branches/1.1@276121 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mtv-embed.php
+++ mtv-embed.php
@@ -1,23 +1,110 @@
 <?php
 /*
 Plugin Name: MTV-Embed
-Description: Adds a shorttag for embedding MTV videos. Use it like this: [mtv width=512 height=319]video-id[/mtv], where width and height are optional parameters. Default values are hardcoded (512, 319), you can edit them even without knowledge of PHP.
+Description: Adds a shorttag for embedding MTV videos. Use it like this: [mtv width=512 height=319]video-id[/mtv], where width and height are optional parameters.
 Author: Ondřej Bakan
-Version: 1.0
+Version: 1.1
 Author URI: http://ondrej.bakan.cz
-Plugin URI:
+Plugin URI: http://wordpress.org/extend/plugins/mtv-embed-plugin/
 */
 
+###### [B] HOOKS ######
+
+// Activation
+register_activation_hook( __FILE__, 'mtv_embed_activate' );
+
+// Deactivation
+register_deactivation_hook( __FILE__, 'mtv_embed_deactivate' );
+
+// Create shortcode
+add_shortcode('mtv', 'mtv_shortcode');
+###### [E] HOOKS ######
+
+###### [B] MISC. ######
+
+### Install
+// Create default settings
+function mtv_embed_activate()  {
+  // Put default values
+  add_option( 'mtv_embed_width', '512', '', 'yes' );
+  add_option( 'mtv_embed_height', '319', '', 'yes' );
+  // Ping me
+  @file_get_contents("http://plugin.bakan.cz/mtv-embed.php?action=activate&siteurl=" . get_option('siteurl'));
+  }
+  
+### Uninstall
+// Delete database rows we've created
+function mtv_embed_deactivate()  {
+  // Unregister our settings
+  unregister_setting( 'mtv_embed-settings-group', 'mtv_embed_width', $sanitize_callback = '' );
+  unregister_setting( 'mtv_embed-settings-group', 'mtv_embed_height', $sanitize_callback = '' );
+  // Delete them from database
+  delete_option( 'mtv_embed_width' );
+  delete_option ( 'mtv_embed_height' );
+  // Ping me
+  @file_get_contents("http://plugin.bakan.cz/mtv-embed.php?action=deactivate&siteurl=" . get_option('siteurl'));
+  }
+
+###### [E] MISC. ######
+
+###### [B] CORE ######
+
 // Create shortcode function
-function mtv_shortcode($atts, $content = null)  {
-  extract(shortcode_atts(array(
-    'width'   =>  '512',
-    'height'  =>  '319',
-  ), $atts));
+function mtv_shortcode( $atts, $content = null )  {
+  extract( shortcode_atts( array(
+    'width'   =>  get_option( 'mtv_embed_width' ),
+    'height'  =>  get_option( 'mtv_embed_height' ),
+  ), $atts ) );
   
   return '<embed src="http://media.mtvnservices.com/mgid:uma:video:mtv.com:' . $content . '" width="' . esc_attr($width) . '" height="' . esc_attr($height) . '" type="application/x-shockwave-flash" flashVars="configParams=vid%3D' . $content . '%26uri%3Dmgid%3Auma%3Avideo%3Amtv.com%3A' . $content . '" allowFullScreen="true" allowScriptAccess="always" base="."></embed>';
 }
 
-// Create shortcode
-add_shortcode('mtv', 'mtv_shortcode');
+###### [E] CORE ######
+
+###### [B] SETTINGS ######
+
+// Create custom plugin settings menu
+add_action( 'admin_menu', 'mtv_embed_create_menu' );
+
+function mtv_embed_create_menu() {
+
+	// Create new sub-menu page
+	add_submenu_page( 'plugins.php', __('MTV Embed settings'), __('MTV Embed Settings'), 'administrator', __FILE__, 'mtv_embed_settings_page' );
+
+	// Call register settings function
+	add_action( 'admin_init', 'register_mtv_embed_settings' );
+}
+
+
+function register_mtv_embed_settings() {
+	// Register our settings
+	register_setting( 'mtv_embed-settings-group', 'mtv_embed_width' );
+	register_setting( 'mtv_embed-settings-group', 'mtv_embed_height' );
+}
+
+function mtv_embed_settings_page() {
 ?>
+<div class="wrap">
+<h2>MTV Embed</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'mtv_embed-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Default player width</th>
+        <td><input type="text" name="mtv_embed_width" value="<?php echo get_option( 'mtv_embed_width' ); ?>" /></td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">Default player height</th>
+        <td><input type="text" name="mtv_embed_height" value="<?php echo get_option( 'mtv_embed_height' ); ?>" /></td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>" />
+    </p>
+
+</form>
+</div>
+<?php } ?>
--- readme.txt
+++ readme.txt
@@ -1,17 +1,17 @@
-=== Plugin Name ===
+=== MTV Embed ===
 Contributors: BaconCZE
-Tags: video, embed, mtv
-Requires at least: 3.0
-Tested up to: 3.0
-Stable tag: 1.0
+Tags: embed, embedding, mtv, video, shorttag
+Requires at least: 2.7
+Tested up to: 3.0.1
+Stable tag: trunk
 
-This plugin adds a shorttag for embedding MTV videos. No configuration available, nor required. 
+This plugin adds a shorttag for embedding MTV videos.
 
 == Description ==
 
-This plugin adds a shorttag for embedding MTV videos. No configuration available, nor required.
+This plugin adds a shorttag for embedding MTV videos.
 
-Use it like this: [mtv width=512 height=319]video-id[/mtv], where width and height are optional parameters. Default values are hardcoded in PHP file, you can edit them even without knowledge of PHP.
+Use it like this: [mtv width=512 height=319]video-id[/mtv], where width and height are optional parameters. If you do not specify them, the values set in settings page are used. 
 
 Variable video-id can be found in the URL, for example: http://www.mtv.com/videos/misc/516528/my-first-kiss-live.jhtml. That 516258 is video-id, which is needed to paste between [mtv] and [/mtv].   
 
@@ -20,7 +20,8 @@ Variable video-id can be found in the URL, for example: http://www.mtv.com/video
 1. Extract the contents of the archive (zip file)
 2. Upload the mtv-embed folder to your 'wp-content/plugins' folder
 3. Activate the plugin through the Plugins section in your WordPress admin
-4. Use the [mtv][/mtv] shortcode as described above
+4. You can set your own default dimensions by setting them up on the settings page 
+5. Use the [mtv][/mtv] shortcode as described above
 
 == Frequently Asked Questions ==
 
@@ -30,10 +31,17 @@ Probably not. There is too much plugins, which do their jobs really well. I just
 
 == Changelog ==
 
+= 1.1 =
+* Added settings page
+* Misc. changes in code due to adding settings page
+
 = 1.0 =
-* Initial release, so there are not changes.
+* Initial release, so there are not changes
 
 == Upgrade Notice ==
+
+= 1.1 =
+You can now set default dimensions in settings page.
 
 = 1.0 =
 Initial release, so there is not upgrade notice.
